db3a16a66798319096f1fc2ff7aa3abd8095e806
lllvcs <22395524+lllvcs@users.noreply.github.com>
2021-11-16 19:18:09 +0800

    Bug Fixed

--- new_cluster/new_cluster_v2_count.py
+++ new_cluster/new_cluster_v2_count.py
@@ -1,3 +1,4 @@
+# Copyright © 2021 LVCS. All Rights Reserved
 import numpy as np
 import pandas as pd
 import copy
@@ -6,15 +7,14 @@ import copy
 element_limit = 250
 
 # 数据导入
-frame1 = pd.read_csv('test.csv')
-frame2 = copy.deepcopy(frame1)
+frame = pd.read_csv('read.csv')
 
 # 删除多余数据
-del frame1['symbol']
-frame1 = frame1.to_numpy()
+del frame['symbol']
+frame = frame.to_numpy()
 
 # 计算总表格皮尔森相关系数
-pc = np.corrcoef(frame1)
+pc = np.corrcoef(frame)
 pc = np.abs(pc)
 
 # 删除对角线
@@ -32,7 +32,7 @@ pc_find = copy.deepcopy(pc)
 pc_find = np.tril(pc_find)
 
 # 聚类分组初始化
-cluster = np.empty([10000, element_limit], dtype=int)
+cluster = np.zeros([10000, element_limit], dtype=int)
 
 i = 0
 for i in range(10000):
@@ -42,15 +42,15 @@ for i in range(10000):
 
     # 标记矩阵中无剩余元素，循环跳出，将所有聚类结果输出
     if x == 0:
-        np.savetxt('result50-1_cluster_250.csv', cluster, delimiter=',')
+        np.savetxt('result.csv', cluster, delimiter=',', fmt='%d')
         break
 
     # 查找最大相关系数的坐标
-    max_peer = np.where(pc_temp == np.max(pc_temp))
+    max_peer = np.where(pc_find == np.max(pc_find))
 
     # 最大相关系数对写入聚类分组
-    cluster[i][0] = max_peer[0][0]
-    cluster[i][1] = max_peer[1][0]
+    cluster[i, 0] = max_peer[0][0]
+    cluster[i, 1] = max_peer[1][0]
 
     # 在标记矩阵中移除写入聚类分组的数据
     pc_find[max_peer[0][0]] = 0
@@ -70,14 +70,14 @@ for i in range(10000):
 
         # 聚类内元素对应相关系数行相加
         for j in range(times):  # j
-            group = group + pc_temp[cluster[i][j]]
+            group = group + pc_temp[cluster[i, j]]
         group = group / times
 
         # 查找外部相关系数最大的元素
         max_peer_next = np.where(group == np.max(group))[0][0]
 
         # 将该元素加入聚类内
-        cluster[i][times] = max_peer_next
+        cluster[i, times] = max_peer_next
 
         # 在标记矩阵中移除该加入聚类的元素
         pc_find[max_peer_next] = 0
